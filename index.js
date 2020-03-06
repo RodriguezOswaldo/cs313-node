1f6822b313b7e30ff41bb007fafc1076f9a2942c
rodriguezoswaldo <owner2303@gmail.com>
2020-03-06 17:22:51 -0500

    post method /calculate

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.urlencoded({extended: true}))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/home', HelloWorld)
-  .get('/calculate', calculate)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  function calculate(req, res){
+  .get('/home', getData)
+  .post('/calculate', function calculate(req, res){
     console.log('you are in here actually');
     res.render('pages/price');
-  }
-  function HelloWorld(req, res){
+    res.end();
+    // res.render('pages/price');
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  // function calculate(req, res){
+  //   console.log('you are in here actually');
+  //   res.render('pages/price');
+  // }
+  function getData(req, res){
     console.log('You are here!');
     res.render('pages/home');
   }
