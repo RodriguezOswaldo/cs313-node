1badda35f036f563e40d293e5b6e0f4f32254b31
rodriguezoswaldo <owner2303@gmail.com>
2020-03-06 12:21:22 -0500

    adding the home view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
+var bodyParser = require("body-parser");
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -9,4 +10,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/home', HelloWorld)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  function HelloWorld(req, res){
+    console.log('You are here!');
+    res.render('pages/home');
+  }
