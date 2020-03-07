624ee3ff29b28efddebcff2cbd787a804715a238
rodriguezoswaldo <owner2303@gmail.com>
2020-03-07 05:46:07 -0500

    fixing conditionals

--- logic.js
+++ logic.js
@@ -1,31 +1,33 @@
 exports.test1 = function(weight, type){
     var price = "";
     switch (type) {
-        case 'stamped':
-        if(weight > 0 && weight < 12){
-            price = 0.25;
-        }else{
-            price = 0.50;
-        }
+        case 'Letters (Stamped)':
+            if(weight > 0 && weight < 3.5){
+                price = "Your package price is: $" + 0.55 * weight;
+            }else{
+                price = "Incorrect weight";
+            }
         break;
-        case 'mettered':
-        if(weight > 0 && weight < 12){
-            price = 0.65;
-        }else{
-            price= 1.25;
-        }
-        case 'envelopes':
-        if(weight > 0 && weight < 12){
-            price = 26.35;
-        }else{
-            price = 36.21;
-        }
-        case 'firstclass':
-        if(weight > 0 && weight < 12){
-            price = 10.75;
-        }else{
-            price = 36.25;
-        }
+        case 'Letters (Metered)':
+            if(weight > 0 && weight < 3.5){
+                price = "Your package price is: $" + 0.50 * weight;
+            }else{
+                price = "Incorrect weight, letter is too heavy.";
+            }
+        break;
+        case 'Large Envelopes (Flats)':
+            if(weight > 0 && weight < 14){
+                price = "Your package price is: $" + 1.10 * weight;
+            }else{
+                price = "Incorrect weight, envelope is too heavy.";
+            }
+        break;
+        case 'First-Class Package Service—Retail':
+            if(weight > 0){
+                price = "Your package price is: $" + 3.80 * weight;
+            }else{
+                price = "Incorrect weight";
+            }
         break;
         default:
             price = 0;
